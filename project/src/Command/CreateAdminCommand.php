ff572bda6a5573d004770056ae188f73793f7ff0
MickeySaltis <cogeta4@hotmail.fr>
2023-09-21 18:09:39 +0200

    Auth

---
+++ project/src/Command/CreateAdminCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\CreateAdminService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Create a new admin user',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly CreateAdminService $createAdminService
+    )
+    {
+        parent::__construct();
+
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'Email of the admin user')
+            ->addArgument('password', InputArgument::REQUIRED, 'Password of the admin user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $pseudo = $input->getArgument('pseudo');
+        $password = $input->getArgument('password');
+
+        $this->createAdminService->create($email, $pseudo, $password);
+
+        $io->success('Successfully created admin user.');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ project/src/Service/CreateAdminService.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class CreateAdminService
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    )
+    {
+
+    }
+
+    public function create(string $email, string $pseudo, string $password):void
+    {
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+
+        if(!$user)
+        {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setPseudo($pseudo);
+
+            $password = $this->passwordHasher->hashPassword($user, $password);
+
+            $user->setPassword($password);
+        }
+
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $this->userRepository->save($user, true);
+    }
+}
